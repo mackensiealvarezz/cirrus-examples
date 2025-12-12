822fd655c666e3839df1ae6a9f9d78bf02072b79
Mackensie Alvarez <greymattermack@gmail.com>
2025-12-12 18:55:20 -0500

    Add variable 'tesst' for resource group location

--- Terraform/Azure/AKS Cluster Windows/variables.tf
+++ Terraform/Azure/AKS Cluster Windows/variables.tf
@@ -10,6 +10,12 @@ variable "test" {
   description = "Location of the resource group."
 }
 
+variable "tesst" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
 variable "resource_group_name_prefix" {
   type        = string
   default     = "rg"
