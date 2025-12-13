8276cd68c7b2b743a199363b3f00d1c4c26852e5
Mackensie Alvarez <greymattermack@gmail.com>
2025-12-12 19:39:01 -0500

    Update variables.tf

--- Terraform/Azure/AKS Cluster Windows/variables.tf
+++ Terraform/Azure/AKS Cluster Windows/variables.tf
@@ -16,6 +16,12 @@ variable "tesst" {
   description = "Location of the resource group."
 }
 
+variable "tessst" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
 variable "resource_group_name_prefix" {
   type        = string
   default     = "rg"
